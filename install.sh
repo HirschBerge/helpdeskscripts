7f8d03c6aa6aad55e5713f413bdd7d7c064aa63a
Hirschy <53668025+HirschBerge@users.noreply.github.com>
2020-03-09 11:25:39 -0400

    Added an install.sh
    added an install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "Changing permissions..."
+sleep 1
+chmod 775 -R $PWD/*
+chown 1000.1000 -R $PWD/*
+echo "Adding scripts to the Desktop..."
+sleep 1
+for i in `ls -1|grep -i -v lic`
+do 
+	ln -s $PWD/$i ~/Desktop/$i
+	sleep 1
+done
+echo "Creating alias 'wipe' to wipe a drive"
+sleep 1
+echo "alias wipe='~/helpdeskscripts/drivewiper5003.sh'" |tee -a ~/.bashrc
+echo "Creating alias 'MAC' to find the mac address"
+sleep 1
+echo "alias MAC='~/helpdeskscripts/MACFinder.sh.sh'" |tee -a ~/.bashrc
+echo "Creating alias 'newtoolkit' to create a new linux toolkit (do not use this from the actual wiping station. ONLY from one flashdrive to another!!!"
+sleep 5
+echo "alias newtoolkit='~/helpdeskscripts/toolkitcreator.sh'" |tee -a ~/.bashrc&& source ~/.bashrc
